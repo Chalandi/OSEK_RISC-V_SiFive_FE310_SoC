2a43845d3f3be65bc95cf21c406fb06942c36205
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-05-27 14:55:38 +0200

    Avoid client misunderstanding of OS_Schedule()

--- Code/tasks.c
+++ Code/tasks.c
@@ -54,8 +54,28 @@ TASK(T1)
 
 TASK(Idle)
 {
+  const OsEventMaskType OsWaitEventMask = (OsEventMaskType) EVT_DUMMY_LED;
+
+  (void)OS_SetRelAlarm(ALARM_DUMMY_LED, 0, 503);
+
   for(;;)
   {
     OS_Schedule();
+
+    OsEventMaskType Events = (OsEventMaskType) 0U;
+
+    if(E_OK == OS_WaitEvent(OsWaitEventMask))
+    {
+      (void) OS_GetEvent((OsTaskType)Idle, &Events);
+
+      if((Events & EVT_DUMMY_LED) == EVT_DUMMY_LED)
+      {
+        OS_ClearEvent(EVT_DUMMY_LED);
+      }
+    }
+    else
+    {
+      OS_TerminateTask(); // In case of error we switch off the task
+    }
   }
 }
